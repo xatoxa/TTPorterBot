56177003a304a9845ef4f8148ecd88038b8c123b
toxic <anton.xatuncev@mail.ru>
2022-08-05 14:36:10 +0300

    increase stability

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 import re
-from tiktok_downloader import services
+from tiktok_downloader import services, InvalidUrl, info_post
 from config import TOKEN
 
 # передача токена
@@ -10,40 +10,34 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	bot.send_message(message.chat.id, 'Привет! Скопируй мне ссылку на видео из ТТ, и я тебе его перезалью.')
+    bot.send_message(message.chat.id, 'Привет! Скопируй мне ссылку на видео из ТТ, и я тебе его перезалью.')
 
 @bot.message_handler(commands=['help'])
 def help(message):
-	bot.send_message(message.chat.id, 'Копируешь в текстовое поле внизу ссылку из тиктока, если ссылка корректная, '
-									  'я скину видео. В зависимости от веса, видео может загружаться даже несколько минут'
-									  ', имей терпение. Когда пройдёт примерно 50% времени загрузки, я тебя уведомлю.')
+    bot.send_message(message.chat.id, 'Копируешь в текстовое поле внизу ссылку из тиктока, если ссылка корректная, '
+                                      'я скину видео. В зависимости от веса, видео может загружаться даже несколько минут'
+                                      ', имей терпение. Когда пройдёт примерно 50% времени загрузки, я тебя уведомлю.')
 
 
 @bot.message_handler()
 def load_video(message):
-	if message.text.startswith('https://www.tiktok.com') or re.search(r'https://v\w\.tiktok\.com*', message.text):
-		url = message.text
-		bot.send_message(message.chat.id, 'Ну-ка что там у тебя...')
-		try:
-			path = f"D:\python dump\{message.from_user.id}.mp4"
-			downloader(url, path)
-			bot.send_message(message.chat.id, 'Ещё немного и видео загрузится!')
-			video = open(path, 'rb')
-			bot.send_video(message.chat.id, video)
-			video.close()
-			os.remove(path)
-		except:
-			bot.send_message(message.chat.id, 'Возникла какая-то ошибка :(\nПроверь ссылку и попробуй ещё раз.')
-	else:
-		bot.send_message(message.chat.id, "Я тебя не понимаю :-(")
-
-
-def downloader(url, path):
-	for service in services.values():
-		media = service(url)
-		if len(media) > 0:
-			media[0].download(path)
-			break
+    if message.text.startswith('https://www.tiktok.com') or re.search(r'https://v\w\.tiktok\.com*', message.text):
+        url = message.text
+        bot.send_message(message.chat.id, 'Ну-ка что там у тебя...')
+        try:
+            path = f"D:\python dump\{message.from_user.id}.mp4"
+            #path = f"/home/tt_porter_bot/tt_porter_bot/python_dump/{message.from_user.id}.mp4"
+            info_post.service(url)[0].download(path)
+            bot.send_message(message.chat.id, 'Ещё немного и видео загрузится!')
+            video = open(path, 'rb')
+            bot.send_video(message.chat.id, video)
+            video.close()
+            os.remove(path)
+        except Exception as error:
+            bot.send_message(message.chat.id, 'Возникла какая-то ошибка :(\nПроверь ссылку и попробуй ещё раз.\n')
+            print(error)
+    else:
+        bot.send_message(message.chat.id, "Я тебя не понимаю :-(")
 
 
 bot.polling(none_stop=True)
